32cda7d698afb031c691b16df28126c7cf7aa8f2
Grégory Le Garec <gregory.legarec@gmail.com>
2017-10-09 21:52:41 +0200

    refactor: better getFileName 🎨

--- utils.js
+++ utils.js
@@ -1,14 +1,9 @@
 export const getFileName = (doc) => {
-  let result = null
-
   if (doc.invoice) {
     // a lot of invoices have the name Facture.pdf. I try to construct a more meaningfull and
     // unique name with invoice information
-    let date = new Date(doc.invoice_data.due_on)
-    date = date.toLocaleDateString()
-    result = `Facture_${date}_${doc.invoice_data.chargeable_amount}${doc.invoice_data.currency}.pdf`
-  } else {
-    result = doc.filename
+    const date = new Date(doc.invoice_data.due_on)
+    return `Facture_${date.toLocaleDateString()}_${doc.invoice_data.chargeable_amount}${doc.invoice_data.currency}.pdf`
   }
-  return result
+  return doc.filename
 }
