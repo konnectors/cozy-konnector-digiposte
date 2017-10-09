c786ad459c5b5da35507bc40218200d8eebbf902
Grégory Le Garec <gregory.legarec@gmail.com>
2017-10-09 22:19:05 +0200

    feat: add dates to every files + tests ✨☔️

---
+++ test/utils.spec.js
@@ -0,0 +1,42 @@
+/* eslint-env jest */
+import { getFileName } from '../utils'
+
+describe('getFileName', () => {
+  it('inserts date into bill', () => {
+    expect(getFileName({
+      invoice: true,
+      invoice_data: {
+        chargeable_amount: '42',
+        currency: '€',
+        due_on: '2017-10-09T16:12:27.704Z'
+      }
+    })).toMatchSnapshot()
+  })
+
+  it('inserts date into account situations', () => {
+    expect(getFileName({
+      filename: 'Relevé de compte.pdf',
+      creation_date: '2017-10-09T16:12:27.704Z'
+    })).toMatchSnapshot()
+  })
+
+  it('inserts date into file with no extensions', () => {
+    expect(getFileName({
+      filename: 'Décidément rien ne va',
+      creation_date: '2017-10-09T16:12:27.704Z'
+    })).toMatchSnapshot()
+  })
+
+  it('returns file name when no date', () => {
+    expect(getFileName({
+      filename: 'Relevé de compte.pdf'
+    })).toMatchSnapshot()
+  })
+
+  it('returns file name when incorrect date', () => {
+    expect(getFileName({
+      filename: 'Relevé de compte.pdf',
+      creation_date: 'Oops not a date'
+    })).toMatchSnapshot()
+  })
+})
--- utils.js
+++ utils.js
@@ -1,9 +1,39 @@
-export const getFileName = (doc) => {
+const getFileName = (doc) => {
   if (doc.invoice) {
     // a lot of invoices have the name Facture.pdf. I try to construct a more meaningfull and
     // unique name with invoice information
     const date = new Date(doc.invoice_data.due_on)
     return `Facture_${date.toLocaleDateString()}_${doc.invoice_data.chargeable_amount}${doc.invoice_data.currency}.pdf`
   }
-  return doc.filename
+
+  // Every file seems to have the same name, so let's try to add the creation
+  // date to every one of them.
+  if (!doc['creation_date']) {
+    return doc.filename
+  }
+
+  const creationDate = new Date(doc['creation_date'])
+  const isValidDate = !(isNaN(creationDate.getTime()))
+
+  if (!isValidDate) {
+    return doc.filename
+  }
+
+  return doc.filename.split('.').reduce((accumulator, fragment, index, fragments) => {
+    const hasNoFileExtension = fragments.length === 1
+    if (hasNoFileExtension) {
+      return `${fragment}_${creationDate.toLocaleDateString()}`
+    }
+
+    const isFileExtension = index === fragments.length - 1
+    if (isFileExtension) {
+      return `${accumulator}_${creationDate.toLocaleDateString()}.${fragment}`
+    }
+
+    return `${accumulator}${index ? '.' : ''}${fragment}`
+  }, '')
+}
+
+module.exports = {
+  getFileName: getFileName
 }
