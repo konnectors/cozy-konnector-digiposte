e6e36548fa5bfb96368b430a45caa3c1fa73a53a
Grégory Le Garec <gregory.legarec@gmail.com>
2017-10-10 16:25:05 +0200

    fix: use date format not depending to locale or OS 🚑

--- utils.js
+++ utils.js
@@ -1,9 +1,13 @@
+const { format } = require('date-fns')
+
+const fileDateFormat = (date) => format(date, 'YYYY-MM-D')
+
 const getFileName = (doc) => {
   if (doc.invoice) {
     // a lot of invoices have the name Facture.pdf. I try to construct a more meaningfull and
     // unique name with invoice information
     const date = new Date(doc.invoice_data.due_on)
-    return `Facture_${date.toLocaleDateString()}_${doc.invoice_data.chargeable_amount}${doc.invoice_data.currency}.pdf`
+    return `Facture_${fileDateFormat(date)}_${doc.invoice_data.chargeable_amount}${doc.invoice_data.currency}.pdf`
   }
 
   // Every file seems to have the same name, so let's try to add the creation
@@ -22,12 +26,12 @@ const getFileName = (doc) => {
   return doc.filename.split('.').reduce((accumulator, fragment, index, fragments) => {
     const hasNoFileExtension = fragments.length === 1
     if (hasNoFileExtension) {
-      return `${fragment}_${creationDate.toLocaleDateString()}`
+      return `${fragment}_${fileDateFormat(creationDate)}`
     }
 
     const isFileExtension = index === fragments.length - 1
     if (isFileExtension) {
-      return `${accumulator}_${creationDate.toLocaleDateString()}.${fragment}`
+      return `${accumulator}_${fileDateFormat(creationDate)}.${fragment}`
     }
 
     return `${accumulator}${index ? '.' : ''}${fragment}`
